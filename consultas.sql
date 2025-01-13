72c3bdb937ce74a66a7354f28f29020a49ba7ba3
Gleytton <gleytton@gmail.com>
2025-01-13 13:41:50 -0300

    Alter consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,43 +1,43 @@
 -- 1- consulta: Obter a data de entrega feita por um fornecedor X que contenha um produto Y
 
- SELECT e_data_ent FROM entrega AS e, entrega_produto AS ep WHERE e_cod = ep_cod_entrega; 
+ SELECT e.data_ent FROM entrega AS e, entrega_produto AS ep WHERE e.cod = ep.cod_entrega; 
 
 -- #2-  consulta: Verificar a quantidade em estoque de um produto X
 
- SELECT p_quant FROM produto AS p WHERE p_nome = "?" ;
+ SELECT p.quant FROM produto AS p WHERE p.nome = "?" ;
 
 -- #3- consulta: Obter quantidade de atrasos de um funcionário X em no dia
 
- SELECT c_nome, c_ponto FROM colaborador As c WHERE c_ponto >"2024-01-01 9:00";
+ SELECT c.nome, c.ponto FROM colaborador As c WHERE c.ponto >"2024-01-01 9:00";
 
 --#4- Consulta: Retornar o CNPJ de todos os fornecedores contratados pelo gerente x na data y
 
- SELECT c_cnpj FROM contrata AS c WHERE c_cpf_gerente = "x" AND c_data = 'y'
+ SELECT c.cnpj FROM contrata AS c WHERE c.cpf_gerente = "x" AND c.data = 'y';
 
 -- #5- Retorna todos os codigos de entregas de um produto
 
-SELECT ep_cod_entrega FROM entrega_produto AS ep , produto AS p WHERE ep_cod_prod = p_cod
+SELECT ep.cod_entrega FROM entrega_produto AS ep , produto AS p WHERE ep.cod_prod = p.cod;
 
 -- #6- Retorna o cliente com maior numero de pontos no cartão fidelidade e o número de pontos:
 
-SELECT c_nome, c_pontos FROM cliente AS c WHERE c_pontos = MAX(c_pontos)
+SELECT c.nome, MAX(c_pontos) AS MAIOR_PONTO FROM cliente AS c;
 
 -- #7- Quantidade de produtos vendidos em uma venda X
 
-SELECT vp_quant FROM venda_produto AS vp WHERE vp_cod = 'x'
+SELECT vp.quant FROM venda_produto AS vp WHERE vp.cod = 'x';
 
 -- #8- Retorna cpf dos funcionários que tem a função X
 
-SELECT cf_cpf FROM colaborador_funcionario AS cf WHERE cf_funcao = 'x'
+SELECT cf.cpf FROM colaborador_funcionario AS cf WHERE cf.funcao = 'x';
 
 #9- Retorna as formas de pagamento cadastradas por um cliente
 
 SELECT fp.tipo
-FROM cliente AS cl JOIN cadastrar AS cad ON cl_cod = cad_cod_cliente
-JOIN forma_pagamento AS fp ON cl_cod = fp_cod_cliente
-WHERE cl_nome  = "x"
+FROM cliente AS cl JOIN cadastrar AS cad ON cl.cod = cad.cod_cliente
+JOIN forma_pagamento AS fp ON cl.cod = fp.cod_cliente
+WHERE cl.nome  = "x";
 
 -- #10- Retorna o produto mais vendido
 
-SELECT p_nome FROM produto AS p WHERE p_quant = MAX(p_quant) 
+SELECT p.nome, MAX(p.quant) AS MAIOR_QUANTIDADE FROM produto AS p;  
 
